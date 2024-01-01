000ec2716d8d3f66f5e5985b77842a9dbb888632
AVI TOMAR <78687121+tomaravi7@users.noreply.github.com>
2024-01-01 09:28:47 +0530

    added delete button and filter

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -96,6 +96,7 @@ class _HomePageState extends State<HomePage> {
   String newTitle = "";
   String newDesc = "";
   bool showDone = false;
+  bool isrev = false;
   void addList() {
     setState(() {
       showCupertinoDialog(
@@ -159,10 +160,12 @@ class _HomePageState extends State<HomePage> {
                         backgroundColor: Colors.green[50]),
                     onPressed: () {
                       setState(() {
-                        todoList.add(Todo(
-                            title: newTitle,
-                            description: newDesc,
-                            isDone: false));
+                        todoList.insert(
+                            0,
+                            Todo(
+                                title: newTitle,
+                                description: newDesc,
+                                isDone: false));
                         newTitle = "";
                         newDesc = "";
                         saveTodos();
@@ -202,101 +205,148 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           backgroundColor: Colors.black,
           bottomOpacity: 20,
-          title: ShaderMask(
-              blendMode: BlendMode.srcIn,
-              shaderCallback: (rect) => const RadialGradient(
-                    center: Alignment.bottomCenter,
-                    radius: 1.85,
-                    colors: <Color>[
-                      Color(0xFF3ecfb2),
-                      Color.fromARGB(255, 0, 255, 204)
-                    ],
-                  ).createShader(rect),
-              child: Title(
-                color: Colors.white,
-                child: const Text('TASKS'),
-              )),
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.menu,
-              color: Color(0xFF3ecfb2),
-            ),
-          ),
+          title: Center(
+              child: ShaderMask(
+                  blendMode: BlendMode.srcIn,
+                  shaderCallback: (rect) => const RadialGradient(
+                        center: Alignment.bottomCenter,
+                        radius: 1.85,
+                        colors: <Color>[
+                          Color(0xFF3ecfb2),
+                          Color.fromARGB(255, 0, 255, 204)
+                        ],
+                      ).createShader(rect),
+                  child: Title(
+                    color: Colors.white,
+                    child: const Text('TASKS'),
+                  ))),
           actions: [
             IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.filter_alt_rounded,
-                color: Color(0xFF3ecfb2),
-              ),
-            ),
+                onPressed: () {
+                  setState(() {
+                    if (isrev) {
+                      todoList.sort((a, b) => a.title.compareTo(b.title));
+                      isrev = false;
+                    } else {
+                      todoList.sort((a, b) => b.title.compareTo(a.title));
+                      isrev = true;
+                    }
+                  });
+                },
+                icon: isrev
+                    ? const Icon(
+                        Icons.filter_list_off_sharp,
+                        color: Color(0xFF3ecfb2),
+                      )
+                    : const Icon(
+                        Icons.filter_list,
+                        color: Color(0xFF3ecfb2),
+                      )),
             if (todoList.where((element) => element.isDone).isNotEmpty)
-              TextButton(
+              IconButton(
                 onPressed: () {
                   setState(() {
                     showDone = !showDone;
                   });
                 },
-                child: showDone
+                icon: showDone
                     ? const Icon(Icons.visibility_off_outlined,
                         color: Color(0xFF3ecfb2))
                     : const Icon(
                         Icons.visibility_outlined,
                         color: Color(0xFF3ecfb2),
                       ),
-              )
+              ),
+            IconButton(
+                onPressed: () {
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                            contentPadding: const EdgeInsets.all(20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              side: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            title: const Text("Delete All Tasks?"),
+                            content: const Text(
+                                "Are you sure you want to delete all tasks?"),
+                            actions: [
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      foregroundColor: Colors.red,
+                                      backgroundColor: Colors.red[50]),
+                                  onPressed: () {
+                                    setState(() {
+                                      Navigator.pop(context);
+                                    });
+                                  },
+                                  child: const Text("Cancel")),
+                              TextButton(
+                                  style: TextButton.styleFrom(
+                                      foregroundColor: Colors.green,
+                                      backgroundColor: Colors.green[50]),
+                                  onPressed: () {
+                                    setState(() {
+                                      resetList();
+                                      Navigator.pop(context);
+                                    });
+                                  },
+                                  child: const Text("Delete")),
+                            ]);
+                      });
+                },
+                icon: const Icon(Icons.delete_forever_outlined,
+                    color: Color(0xFF3ecfb2)))
           ],
         ),
         body: Align(
           alignment: Alignment.topCenter,
-          child: Container(
-            constraints: const BoxConstraints(maxHeight: 400),
-            child: ListView.builder(
-                itemCount: todoList.length,
-                itemBuilder: (context, index) {
-                  if (todoList[index].isDone && !showDone) return Container();
-                  return ListTile(
-                    title: Text(
-                      todoList[index].title,
-                      style: TextStyle(
-                          decoration: todoList[index].isDone
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none,
-                          decorationColor: const Color(0xFF3ecfb2),
-                          decorationThickness: 5,
-                          decorationStyle: TextDecorationStyle.wavy,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: todoList[index].description != null
-                        ? Text(
-                            todoList[index].description!,
-                            style: TextStyle(
-                                decoration: todoList[index].isDone
-                                    ? TextDecoration.lineThrough
-                                    : TextDecoration.none,
-                                decorationColor: const Color(0xFF3ecfb2),
-                                decorationThickness: 5,
-                                fontWeight: FontWeight.bold),
-                          )
-                        : null,
-                    trailing: Checkbox(
-                      value: todoList[index].isDone,
-                      onChanged: (bool? value) {
-                        updateTodo(index, value);
-                        todoList.sort((a, b) {
-                          if (a.isDone && !b.isDone) return 1;
-                          if (!a.isDone && b.isDone) return -1;
-                          return 0;
-                        });
-                      },
-                      activeColor: const Color(0xFF3ecfb2),
-                    ),
-                    onLongPress: () => deleteTodo(index),
-                  );
-                }),
-          ),
+          child: ListView.builder(
+              itemCount: todoList.length,
+              itemBuilder: (context, index) {
+                if (todoList[index].isDone && !showDone) return Container();
+                return ListTile(
+                  title: Text(
+                    todoList[index].title,
+                    style: TextStyle(
+                        decoration: todoList[index].isDone
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        decorationColor: const Color(0xFF3ecfb2),
+                        decorationThickness: 5,
+                        decorationStyle: TextDecorationStyle.wavy,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: todoList[index].description != null
+                      ? Text(
+                          todoList[index].description!,
+                          style: TextStyle(
+                              decoration: todoList[index].isDone
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                              decorationColor: const Color(0xFF3ecfb2),
+                              decorationThickness: 5,
+                              fontWeight: FontWeight.bold),
+                        )
+                      : null,
+                  trailing: Checkbox(
+                    value: todoList[index].isDone,
+                    onChanged: (bool? value) {
+                      updateTodo(index, value);
+                      todoList.sort((a, b) {
+                        if (a.isDone && !b.isDone) return 1;
+                        if (!a.isDone && b.isDone) return -1;
+                        return 0;
+                      });
+                    },
+                    activeColor: const Color(0xFF3ecfb2),
+                  ),
+                  onLongPress: () => deleteTodo(index),
+                );
+              }),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: addList,
